ee5a5d0fc7c7e687b19496d7fc3446f24446e410
Abdur Rafi <rafiridwan23@gmail.com>
2023-11-24 09:44:14 +0700

    feat(laravel ui): auth system

--- routes/web.php
+++ routes/web.php
@@ -87,3 +87,6 @@ Route::get('/satwa', function () {
 Route::get('/laporkan', function () {
     return view('laporkan');
 }); 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
