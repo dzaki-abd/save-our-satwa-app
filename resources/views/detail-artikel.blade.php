c4f048f6d9c00775532e7231dd043359366bc899
alyaiz <alyaizzahzalfa@gmail.com>
2023-11-30 10:21:55 +0700

    feat integration : list article and detail atricle

--- resources/views/detail-artikel.blade.php
+++ resources/views/detail-artikel.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app', ['showFooter' => true])
 
 @section('jumbotron')
-  <div class="jumbotron-4 jumbotron-search-4" style="background-image: url('../img/donasi.jpg')">
+  <div class="jumbotron-4 jumbotron-search-4" style="background-image: url('{{ asset('storage/img/artikel_images/' . $artikel->gambar) }}')">
     <div class="jumbotron-container-4">
       <div class="container h-100">
         <div class="d-flex align-items-center justify-content-center h-100 jumbotron-content-4">
           <div class="d-flex flex-column align-items-start">
-            <h2 class="text-white mb-1">Mengenal Satwa Kakaktua Raja</h2>
+            <h2 class="text-white mb-1">{{ $artikel->judul }}</h2>
             <nav aria-label="breadcrumb" data-bs-theme="dark">
               <ol class="breadcrumb m-0">
                 <li class="breadcrumb-item"><a href="/">Home</a></li>
@@ -22,122 +22,35 @@
     </div>
   </div>
 @endsection
-
 @section('content')
   <div class="container shadow rounded detail-artikel-container p-2 p-md-3">
-    <a href="../img/donasi.jpg" data-fancybox>
-      <img src="../img/donasi.jpg" class="rounded" alt="">
+    <a href="{{ asset('storage/img/artikel_images/' . $artikel->gambar) }}" data-fancybox>
+      <img src="{{ asset('storage/img/artikel_images/' . $artikel->gambar) }}" class="rounded" alt="">
     </a>
 
     <div class="row g-0 mt-4">
-      <div class="col-md-9 detail-artikel-content">
-        <h3> heading 3</h3>
-        <p>Harimau Sumatra (Panthera tigris sumatrae) merupakan salah satu spesies harimau yang mendiami pulau Sumatra, Indonesia. Keunikan dan keindahan harimau ini membuatnya menjadi objek kajian ilmiah, kebanggaan nasional, dan kepedulian global terkait pelestarian satwa liar. Namun, sayangnya, populasinya kini terancam punah akibat berbagai ancaman&nbsp;dari&nbsp;manusia.</p>
-        <p>Harimau Sumatra (Panthera tigris sumatrae) merupakan salah satu spesies harimau yang mendiami pulau Sumatra, Indonesia. Keunikan dan keindahan harimau ini membuatnya menjadi objek kajian ilmiah, kebanggaan nasional, dan kepedulian global terkait pelestarian satwa liar. Namun, sayangnya, populasinya kini terancam punah akibat berbagai ancaman&nbsp;dari&nbsp;manusia.</p>
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>heading 3</h3>
-        <p>paragraf</p>
-        <p><strong>Bold</strong></p>
-        <p><i>italic</i></p>
-        <ul>
-          <li>
-            Soto&nbsp;
-          </li>
-          <li>
-            Pecel
-          </li>
-          <li>
-            Rawon
-          </li>
-        </ul>
-        <ol>
-          <li>
-            Soto&nbsp;
-          </li>
-          <li>
-            Pecel
-          </li>
-          <li>
-            Rawon
-          </li>
-        </ol>
-        <blockquote>
-          <p>ini Blockquote</p>
-        </blockquote>
-        <p><a href="http://127.0.0.1:8000/laporkan">Ini link</a></p>
-        <h3>heading 3</h3>
-        <p>Harimau Sumatra (Panthera tigris sumatrae) merupakan salah satu spesies harimau yang mendiami pulau Sumatra, Indonesia. Keunikan dan keindahan harimau ini membuatnya menjadi objek kajian ilmiah, kebanggaan nasional, dan kepedulian global terkait pelestarian satwa liar. Namun, sayangnya, populasinya kini terancam punah akibat berbagai ancaman&nbsp;dari&nbsp;manusia.</p>
-        <p>Harimau Sumatra (Panthera tigris sumatrae) merupakan salah satu spesies harimau yang mendiami pulau Sumatra, Indonesia. Keunikan dan keindahan harimau ini membuatnya menjadi objek kajian ilmiah, kebanggaan nasional, dan kepedulian global terkait pelestarian satwa liar. Namun, sayangnya, populasinya kini terancam punah akibat berbagai ancaman&nbsp;dari&nbsp;manusia.</p>
-
-        <h3>heading 3</h3>
-        <p>Harimau Sumatra (Panthera tigris sumatrae) merupakan salah satu spesies harimau yang mendiami pulau Sumatra, Indonesia. Keunikan dan keindahan harimau ini membuatnya menjadi objek kajian ilmiah, kebanggaan nasional, dan kepedulian global terkait pelestarian satwa liar. Namun, sayangnya, populasinya kini terancam punah akibat berbagai ancaman&nbsp;dari&nbsp;manusia.</p>
-        <p>Harimau Sumatra (Panthera tigris sumatrae) merupakan salah satu spesies harimau yang mendiami pulau Sumatra, Indonesia. Keunikan dan keindahan harimau ini membuatnya menjadi objek kajian ilmiah, kebanggaan nasional, dan kepedulian global terkait pelestarian satwa liar. Namun, sayangnya, populasinya kini terancam punah akibat berbagai ancaman&nbsp;dari&nbsp;manusia.</p>
-        <figure class="table">
-          <table>
-            <thead>
-              <tr>
-                <th>
-                  No
-                </th>
-                <th>
-                  Nama
-                </th>
-                <th>
-                  Nama
-                </th>
-                <th>
-                  Nama
-                </th>
-                <th>
-                  Nama
-                </th>
-                <th>
-                  Nama
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  1
-                </td>
-                <td>
-                  Kevin Iansyah
-                </td>
-                <td>
-                Kevin Iansyah
-                </td>
-                <td>
-                  Kevin Iansyah
-                </td>
-                <td>
-                  Kevin Iansyah
-                </td>
-                <td>
-                  Kevin Iansyah
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </figure>
+      <div class="col-lg-9 detail-artikel-content pe-0 pe-lg-5">
+        {!! $artikel->konten !!}
       </div>
-      <div class="col-md-3 detail-artikel-sidebar">
+      <div class="col-lg-3 detail-artikel-sidebar">
         <h6>Jenis</h6>
-        <p>Artikel</p>
+        <p>{{ $artikel->jenis }}</p>
         <h6>Penulis</h6>
         <p>Admin</p>
         <h6>Diterbitkan</h6>
-        <p>20 November 2023</p>
+        <p>{{ \Carbon\Carbon::parse($artikel->created_at)->format('d F Y') }}</p>
+        <h6>Diubah</h6>
+        <p>{{ \Carbon\Carbon::parse($artikel->updated_at)->format('d F Y') }}</p>
         <h6>Kata Kunci</h6>
         <div class="d-flex flex-wrap gap-2">
-          <div class="button-teal-500">Satwa</div>
-          <div class="button-teal-500">Burung</div>
-          <div class="button-teal-500">Kakaktua</div>
-          <div class="button-teal-500">Kakaktua Raja</div>
-          <div class="button-teal-500">Dilindungi</div>
-          <div class="button-teal-500">Terancam</div>
-        </div>
+          @php
+              $tags = explode(', ', $artikel->tag);
+          @endphp
+      
+          @foreach ($tags as $tag)
+              <div class="button-teal-500">{{ $tag }}</div>
+          @endforeach
+      </div>
       </div>
     </div>
   </div>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,8 @@
-<div class="footer card text-center p-0 shadow {{ Request::url() == url('/detail-artikel') ? 'active' : '' }}" data-bs-theme="dark">
+@php
+    $id = request()->route('id');
+@endphp
+
+<div class="footer card text-center p-0 shadow {{ Request::url() == route('detail-artikel', ['id' => $id]) ? 'active' : '' }}" data-bs-theme="dark">
   <div class="container">
     <div class="card-body d-flex flex-column justify-content-between gap-4 container flex-lg-row p-0">
       <div class="card border-0" style="min-width: 20rem; width: 100%">
